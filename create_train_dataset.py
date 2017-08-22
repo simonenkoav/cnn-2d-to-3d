Fix projection calculation for about X and Y axes rotations

create_train_dataset.py
@@ -3,6 +3,10 @@ import numpy as np
 import random
 import copy
 import math
+import matplotlib.pyplot as plt
+from enum import Enum
+
+focal_length = 2.41421356
 
 train_size = 10  # count of deviations for one position
 image_side = 20
@@ -12,6 +16,12 @@ train_data = np.zeros(array_form)
 
 image_form = (image_side, image_side)
 empty_image_array = np.zeros(image_form)
+
+
+class Axes(Enum):
+    X = 0
+    Y = 1
+    Z = 2
 
 
 def get_coordinate(start, end):
@@ -39,9 +49,14 @@ def set_points_on_image(image_array, points_coordinates):
     return image_array_with_points
 
 
-def make_rotation_matrix_about_z_axis(degrees):
+def get_sin_cos(degrees):
     theta = np.radians(degrees)
     c, s = np.cos(theta), np.sin(theta)
+    return c, s
+
+
+def make_rotation_matrix_about_z_axis(degrees):
+    c, s = get_sin_cos(degrees)
     R = np.matrix('{} {} {}; {} {} {}; {} {} {}'.format(c, -s, 0,
                                                         s, c, 0,
                                                         0, 0, 1))
@@ -49,22 +64,50 @@ def make_rotation_matrix_about_z_axis(degrees):
 
 
 def make_rotation_matrix_about_x_axis(degrees):
-    theta = np.radians(degrees)
-    c, s = np.cos(theta), np.sin(theta)
+    c, s = get_sin_cos(degrees)
     R = np.matrix('{} {} {}; {} {} {}; {} {} {}'.format(1, 0, 0,
-                                                        0, s, c,
-                                                        0, c, s))
+                                                        0, c, -s,
+                                                        0, s, c))
     return R
 
 
-def rotate(coordinates, rotation_matrix):
-    new_coordinates = copy.deepcopy(np.array(coordinates))
+def make_rotation_matrix_about_y_axis(degrees):
+    c, s = get_sin_cos(degrees)
+    R = np.matrix('{} {} {}; {} {} {}; {} {} {}'.format(c, 0, s,
+                                                        0, 1, 0,
+                                                        -s, 0, c))
+    return R
 
-    for i in range(new_coordinates.shape[0]):
-        point = np.array(new_coordinates[i])
-        xyz = np.array(point)
-        new_coordinates[i] = np.dot(rotation_matrix, xyz)
 
+def add_transpose(R):
+    RT = np.zeros((R.shape[0], R.shape[1] + 1))
+    RT[:, :-1] = R
+    RT[0][-1] = 0
+    RT[1][-1] = 0
+    RT[2][-1] = 5
+    return RT
+
+
+def make_rt_matrix(axis, degrees):
+    if axis == Axes.X:
+        R = make_rotation_matrix_about_x_axis(degrees)
+        return add_transpose(R)
+    elif axis == Axes.Y:
+        R = make_rotation_matrix_about_y_axis(degrees)
+        return add_transpose(R)
+    elif axis == Axes.Z:
+        R = make_rotation_matrix_about_z_axis(degrees)
+        return add_transpose(R)
+
+
+# coordinates should be passed in form [[point0_x, point0_y, point0_z], [point1_x, point1_y, point1_z], ...]
+def transform_to_homogeneous(coordinates):
+    new_coordinates = np.zeros((coordinates.shape[0], coordinates.shape[1] + 1))
+    new_coordinates[:, :-1] = coordinates
+    new_coordinates[0][-1] = 1
+    new_coordinates[1][-1] = 1
+    new_coordinates[2][-1] = 1
+    new_coordinates[3][-1] = 1
     return new_coordinates
 
 
@@ -79,31 +122,30 @@ def generate_rotation_matrices():
     return rotations
 
 
-def make_camera_matrix(initial_fx_in_degrees, image_width, image_height):
-    aspect = image_width / image_height;
-
-    initial_fx_in_radians = np.radians(initial_fx_in_degrees);
-    fy = 1.0 / math.atan(initial_fx_in_radians / 2.0);
-    fx = fy / aspect;
-
-    cx = image_width / 2.0;
-    cy = image_height / 2.0;
-
-    camera_matrix = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
-    return camera_matrix
+def get_projection(point):
+    x = (focal_length / point[-1]) * point[0]
+    y = (focal_length / point[-1]) * point[1]
+    return x, y
 
 
 def get_normal_coordinates(in_vector_coordinates):
     new_coordinates = copy.deepcopy(in_vector_coordinates)
     for point in new_coordinates:
-        for i in range(0, 2):
-            point[i] = (point[i] - 9) / 5
+        point0 = point[0]
+        point1 = point[1]
+        point[0] = (point1 - 9) / 5
+        point[1] = (9 - point0) / 5
 
-    return new_coordinates
+    return np.array(new_coordinates)
+
+
+def get_2d(all_cordinates):
+    coordinates_2d = np.array(all_cordinates[:-1, :]).transpose()
+    return coordinates_2d
 
 
 # square place in the center of image without rotations
-perfect_coordinates = [[4, 4, 0], [4, 14, 0], [14, 4, 0], [14, 14, 0]]  # coordinates of 4 points
+perfect_coordinates = [[4, 14, 0], [4, 4, 0], [14, 4, 0], [14, 14, 0]]  # coordinates of 4 points
 
 
 for i in range(train_size):
@@ -112,58 +154,27 @@ for i in range(train_size):
     train_data[i] = image_with_points
 
 
-perfect_coordinates = [[4, 4, 0], [4, 14, 0], [14, 4, 0], [14, 14, 0]]  # coordinates of 4 points
+perfect_coordinates = [[4, 14, 0], [4, 4, 0], [14, 4, 0], [14, 14, 0]]  # coordinates of 4 points
 
-print("perfect_coords = " + str(perfect_coordinates))
-r = make_rotation_matrix_about_x_axis(45)
-res = rotate(perfect_coordinates, r)
-
-# res = [[2, 2, 2], [2, 7, 2], [14, 4, 0], [14, 14, 0]]
-cam_matrix = make_camera_matrix(45, 2.0, 2.0)
-print("res = " + str(res))
-print("cam_matrix = " + str(cam_matrix))
-res = np.transpose(res)
-res = np.dot(cam_matrix, res)
-print("test rotate about x (45°) = " + str(np.transpose(res)))
-
-r = make_rotation_matrix_about_x_axis(60)
-res = rotate(perfect_coordinates, r)
-res = np.transpose(res)
-res = np.dot(cam_matrix, res)
-print("test rotate about x (60°)= " + str(np.transpose(res)))
 # TODO rotate in cycle: in range 0°-360° with step of 20 for all 3 axes -> 58320 (10*18*18*18) correct images of square
 
-rotations = generate_rotation_matrices()  # around Z axis
 
-rotation_matrix = make_rotation_matrix_about_z_axis(90)
-rotate(perfect_coordinates, rotation_matrix)
-
-# ANOTHER ATTEMPT: (u v) = CAM_MATRIX * RT * XYZ_coordinates
-
-print("\n\n-----")
-rotation_M = make_rotation_matrix_about_x_axis(45)
-rotation_M = np.transpose(rotation_M)
-
-RT = np.zeros((rotation_M.shape[0], rotation_M.shape[1] + 1))
-RT[:, :-1] = rotation_M
-RT[-1][-1] = -2
-
-print("cam_M = " + str(cam_matrix))
+RT = make_rt_matrix(Axes.X, 45)
 print("RT = " + str(RT))
-
-res1 = np.dot(cam_matrix, RT)
-print("res1 = " + str(res1))
-
-normal_coordinates = np.array(get_normal_coordinates(perfect_coordinates))
+normal_coordinates = get_normal_coordinates(perfect_coordinates)
 print("normal_coordinates = " + str(normal_coordinates))
-# new_coords = np.transpose(new_coords)
-points_with_one = np.ones((normal_coordinates.shape[0], normal_coordinates.shape[1] + 1))
-points_with_one[:, :-1] = normal_coordinates
-points_with_one = np.transpose(points_with_one )
-print("points_with_one = " + str(points_with_one))
+normal_coordinates = transform_to_homogeneous(normal_coordinates)
+normal_coordinates = np.transpose(normal_coordinates)
+print("normal_coordinates (homogeneous) = " + str(normal_coordinates))
+rotated = np.dot(RT, normal_coordinates)
+rotated = np.transpose(rotated)
+rotated = np.array(rotated)  # if i won't do it, when iterating each element would be presented in the form [[1 2 3]]
+# i don't know why this shit happens
 
-res2 = np.dot(res1, points_with_one)
-print("res2 = " + str(np.transpose(res2)))
+print("rotated = " + str(rotated))
 
+for point in rotated:
+    point[0], point[1] = get_projection(point)
 
-
+print("rotated (projections) = " + str(rotated))
+print("2d = " + str(get_2d(np.transpose(rotated))))